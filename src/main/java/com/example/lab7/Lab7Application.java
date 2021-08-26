471567fba924145a9fac3c31d1d17c66b7d317d4
Vector2588 <natkamol082@gmail.com>
2021-08-26 15:22:10 +0700

    3.4

--- src/main/java/com/example/lab7/Lab7Application.java
+++ src/main/java/com/example/lab7/Lab7Application.java
@@ -2,10 +2,21 @@ package com.example.lab7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab7Application {
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/events").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(Lab7Application.class, args);
     }
--- src/main/java/com/example/lab7/controller/EventController.java
+++ src/main/java/com/example/lab7/controller/EventController.java
@@ -1,9 +1,13 @@
 package com.example.lab7.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import com.example.lab7.entity.Event;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -19,7 +23,7 @@ public class EventController {
         eventList.add(Event.builder()
                 .id(123L)
                 .category("animal welfare tood")
-                .title("Cat Adoption Day")
+                .title("Cat Adoption Day tood")
                 .description("Find your new feline friend at this event.")
                 .location("Meow Town")
                 .date("January 28, 2022")
@@ -85,7 +89,35 @@ public class EventController {
     }
 
     @GetMapping("events")
-    public ResponseEntity<?> gettEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> gettEventLists(@RequestParam(value = "_limit",
+            required = false) Integer perPage, @RequestParam(value = "_page", required = false) Integer page) {
+        perPage = perPage == null ? eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * perPage;
+        List<Event> output = new ArrayList<>();
+        try {
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(eventList.get(i));
+            }
+            return ResponseEntity.ok(output);
+        } catch (IndexOutOfBoundsException ex) {
+            return ResponseEntity.ok(output);
+        }
+    }
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
+        Event output = null;
+        for (Event event : eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
+            }
+        }
+        if (output != null){
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The given id is not found");
+        }
     }
 }
